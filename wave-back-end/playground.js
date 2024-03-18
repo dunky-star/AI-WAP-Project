e6ba165d631ba8c601bee67b4918b3007f095d0f
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-17 22:43:31 -0500

    OpenAI SDK Integration Successful

--- wave-back-end/playground.js
+++ wave-back-end/playground.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('Hello OpenAI API');
+const readline = require('readline');
+const {
+  generateChatCompletion,
+} = require('./controllers/chatCompletionController');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  'Enter Prompt to Send for GPT3.5 ChatCompletion Model: \n',
+  user_role_content => generateChatCompletion(user_role_content)
+);
