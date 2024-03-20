470ea5b5340d14ce00996be9153b61de4e093ed9
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-20 02:11:50 -0500

    UserReview functionality - work in progress

--- wave-back-end/routes/userRouter.js
+++ wave-back-end/routes/userRouter.js
@@ -8,6 +8,6 @@ router.post('/cookchef/v1/register', userController.userRegistration);
 router.post('/cookchef/v1/login', userController.userLogin);
 router.post('/cookchef/v1/review', isLogin, userController.postUserReview);
 router.get('/cookchef/v1/reviews', isLogin, userController.getUserReviews);
-router.get('/cookchef/v1/resetpassword', userController.resetPassword);
+router.put('/cookchef/v1/resetpassword', userController.resetPassword);
 
 module.exports = router;
