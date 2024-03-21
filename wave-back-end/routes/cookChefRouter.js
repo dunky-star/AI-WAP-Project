4be8f84268501fd8aab31d2203758292502c2bca
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-21 10:07:29 -0500

    Final Backend API

--- wave-back-end/routes/cookChefRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cookChefController = require('../controllers/cookChefController');
-
-router.post('/cookchef/v1/meals', cookChefController.createHealthyMeals);
-
-module.exports = router;
--- wave-back-end/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = id => {
-  return jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: '7d' });
-};
-
-module.exports = generateToken;
