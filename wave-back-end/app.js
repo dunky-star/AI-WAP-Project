6ac02a804ec2cb09e4385ce67d0f4aaad04b6c52
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-20 23:31:48 -0500

    Last changes from Brian.

--- wave-back-end/app.js
+++ wave-back-end/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require("cors");
-const cookieSession = require("cookie-session");
-const dbConfig = require("./config/db.config");
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const dbConfig = require('./config/db.config');
 require('dotenv').config();
 const cookChefRoutes = require('./routes/cookChefRouter');
 //const globalErrHandler = require('./other-errors/globalErrHandler');
@@ -15,49 +15,45 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 // parse requests of content-type - application/x-www-form-urlencoded
 //app.use(express.urlencoded({ extended: true }));
 //jwt token is setting in the cookie
 app.use(
   cookieSession({
-    name: "cookfood-session",
-    keys: ["COOKIE_SECRET"], // should use as secret environment variable
-    httpOnly: true
+    name: 'cookfood-session',
+    keys: ['COOKIE_SECRET'], // should use as secret environment variable
+    httpOnly: true,
   })
 );
 
-
+app.use(cors());
 // Routes import
 app.use(cookChefRoutes);
-app.use(cors());
+
 // simple route
-app.get("/checkapp", (req, res) => {
-  res.json({ message: "Welcome to cook food." });
+app.get('/checkapp', (req, res) => {
+  res.json({ message: 'Welcome to cook food.' });
 });
 // routes
-require("./routes/auth.routes")(app);
-require("./routes/user.routes")(app);
-
-
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
 // it is for mongo DB connection
-const db = require("./models");
+const db = require('./models');
 
 db.mongoose
   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Successfully connect to MongoDB.");
+    console.log('Successfully connect to MongoDB.');
   })
   .catch(err => {
-    console.error("Connection error", err);
+    console.error('Connection error', err);
     process.exit();
   });
 
 app.listen(3000, () =>
   console.log('Server started and listening to requests on port 3000')
 );
-
--- wave-front-end/pages/app.js
+++ wave-front-end/pages/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const router = require('./router');
 const app = express();
+const cors = require('cors');
 
 const PORT = 4000;
+
+// cors middleware
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
