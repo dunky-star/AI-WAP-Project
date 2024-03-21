39f7f129219076dacd4298c8ee6570be9e93039c
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-20 20:32:45 -0500

    Pages from Brian

--- wave-back-end/playground.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const { createHealthyMeals } = require('./controllers/cookChefController');
-const foods = 'broccoli, chicken, fish, vegetables, cabbage, eggs, olive oil';
-createHealthyMeals(foods).then(meals => {
-  console.log(meals);
-});
--- wave-back-end/routes/chatroute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateChatCompletion } = require('../controllers/chatController');
-
-router.post('/cookchef/v1/chatting', generateChatCompletion);
-
-module.exports = router;
