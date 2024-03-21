60fe68472d78eedec9b602696eb44682dc85ecc5
Your Name <you@example.com>
2024-03-20 22:20:36 -0500

    frontEnd finished

--- wave-front-end/pages/router.js
+++ wave-front-end/pages/router.js
@@ -14,43 +14,146 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/login', async (req, res, next) => {
-  console.log('Am in login-route');
+  console.log('Am in login-route', req.body);
   try {
-    const backendResponse = await axios.post(
-      'http://127.0.0.1:3000/api/auth/signin',
-      req.body
-    );
-    // If Successful send the landing page
-    //res.json(backendResponse.data);
-    if (backendResponse) res.sendFile(path.join(__dirname, 'chef_page.html'));
-    else res.sendFile(path.join(__dirname, 'error.html'));
-    console.log(backendResponse.data);
+    let details = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    const response = await fetch('http://localhost:3000/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(details),
+    });
+
+    if (response.ok) {
+      // If Successful send the landing page
+      res.sendFile(path.join(__dirname, 'chef_page.html'));
+    } else {
+      // If there was an error in the request
+      res.sendFile(path.join(__dirname, 'error.html'));
+    }
+
+    console.log(await response.json());
     //res.sendFile(path.join(__dirname, 'chef_page.html'));
   } catch (error) {
     console.error('Error forwarding request to backend:', error.message);
     res.status(500).sendFile(path.join(__dirname, 'error.html'));
   }
 });
+
+// router.post('/login', async (req, res, next) => {
+//   console.log('Am in login-route');
+//   try {
+//     const backendResponse = await axios.post(
+//       'http://127.0.0.1:3000/api/auth/signin',
+//       req.body
+//     );
+//     // If Successful send the landing page
+//     //res.json(backendResponse.data);
+//     if (backendResponse) res.sendFile(path.join(__dirname, 'chef_page.html'));
+//     else res.sendFile(path.join(__dirname, 'error.html'));
+//     console.log(backendResponse.data);
+//     //res.sendFile(path.join(__dirname, 'chef_page.html'));
+//   } catch (error) {
+//     console.error('Error forwarding request to backend:', error.message);
+//     res.status(500).sendFile(path.join(__dirname, 'error.html'));
+//   }
+// });
 
 router.post('/signup', async (req, res, next) => {
-  onsole.log('Am in signup-route');
-  try {
-    const backendResponse = await axios.post(
-      'http://127.0.0.1:3000/api/auth/signup',
-      req.body
-    );
-    // If Successful send the landing page
-    if (backendResponse) res.sendFile(path.join(__dirname, 'chef_page.html'));
-    else res.sendFile(path.join(__dirname, 'error.html'));
-    //res.json(backendResponse.data);
+  console.log('Am in signup-route', req.body);
+  let details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log('details', details);
 
-    console.log(backendResponse.data);
+  try {
+    const response = await fetch('http://localhost:3000/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(details),
+    });
+
+    if (response.ok) {
+      // If Successful send the landing page
+      res.sendFile(path.join(__dirname, 'chef_page.html'));
+    } else {
+      // If there was an error in the request
+      res.sendFile(path.join(__dirname, 'error.html'));
+    }
+
+    const responseData = await response.json();
+    console.log(responseData);
+
     //res.sendFile(path.join(__dirname, 'chef_page.html'));
   } catch (error) {
     console.error('Error forwarding request to backend:', error.message);
     res.status(500).sendFile(path.join(__dirname, 'error.html'));
   }
 });
+
+// router.post('/signup', async (req, res, next) => {
+//   console.log('Am in signup-route', req.body);
+//   let details = {
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname,
+//     email: req.body.email,
+//     password: req.body.password,
+//   };
+//   console.log('details', details);
+//   try {
+//     const backendResponse = await axios.post(
+//       'http://localhost:3000/api/auth/signup',
+//       details
+//     );
+//     // If Successful send the landing page
+//     if (backendResponse) res.sendFile(path.join(__dirname, 'chef_page.html'));
+//     else res.sendFile(path.join(__dirname, 'error.html'));
+//     //res.json(backendResponse.data);
+
+//     console.log(backendResponse.data);
+//     //res.sendFile(path.join(__dirname, 'chef_page.html'));
+//   } catch (error) {
+//     console.error('Error forwarding request to backend:', error.message);
+//     res.status(500).sendFile(path.join(__dirname, 'error.html'));
+//   }
+// });
+
+// router.post('/signup', async (req, res, next) => {
+//   console.log('Am in signup-route', req.body);
+//   let details = {
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname,
+//     email: req.body.email,
+//     password: req.body.password,
+//   };
+//   console.log('details', details);
+//   try {
+//     const backendResponse = await axios.post(
+//       'http://localhost:3000/api/auth/signup',
+//       details
+//     );
+//     // If Successful send the landing page
+//     if (backendResponse) res.sendFile(path.join(__dirname, 'chef_page.html'));
+//     else res.sendFile(path.join(__dirname, 'error.html'));
+//     //res.json(backendResponse.data);
+
+//     console.log(backendResponse.data);
+//     //res.sendFile(path.join(__dirname, 'chef_page.html'));
+//   } catch (error) {
+//     console.error('Error forwarding request to backend:', error.message);
+//     res.status(500).sendFile(path.join(__dirname, 'error.html'));
+//   }
+// });
 
 // router.get('/fail', (req, res, next) => {
 //   counter = counter++;
