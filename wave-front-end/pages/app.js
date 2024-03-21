4801d42e7662adda8cc3ea307188110b3fc099c0
Your Name <you@example.com>
2024-03-20 20:15:12 -0500

    updated with routes

---
+++ wave-front-end/pages/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const router = require('./router');
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
+});
+
+//body parser
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+// Middleware to serve static files
+app.use('/css', express.static(path.join(__dirname, 'css')));
