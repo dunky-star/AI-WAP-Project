a7d842167b0a3f7ca92703d32f0f22dd105096a4
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-23 12:00:37 -0500

    Converting pages to EJS (Embedded Java Script) rendering

--- wave-front-end/app.js
+++ wave-front-end/app.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const path = require('path');
-const router = require('./router');
+const loginRegisterRoutes = require('./routes/login-register-route');
 const app = express();
 const cors = require('cors');
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // cors middleware
 app.use(cors());
 
+// Static files in public directory- css & JavaScript
+app.use(express.static('public'));
+
+// Custom routes
+app.use(loginRegisterRoutes);
+
+// Middleware to serve static files
+//app.use('/css', express.static(path.join(__dirname, 'css')));
+//app.use('/js', express.static(path.join(__dirname, 'js')));
+
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
-
-//body parser
-router.use(express.json());
-router.use(express.urlencoded({ extended: false }));
-app.use(router);
-
-// Middleware to serve static files
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/js', express.static(path.join(__dirname, 'js')));
